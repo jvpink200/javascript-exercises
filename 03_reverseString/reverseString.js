c003cefb5973addb6d500b9d9490bf45fae8de70
User <user@Users-Mac-mini.lan>
2023-03-22 17:19:09 -0700

    reversed string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (word) {
+  let givenWord = word.split("");
 
+  let result = "";
+  for (let i = 0; i < word.length; i++) {
+    result = givenWord.reverse().join("");
+    return result;
+  }
 };
 
 // Do not edit below this line
